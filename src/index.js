f529dc537c7111207932a4f2201db867f842aabb
Marifer Villarroel <mafevito@hotmail.com>
2023-05-15 18:21:53 +0200

    Configuracion react-router basico

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 // Importar estilos de bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+// Configurar react-router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
