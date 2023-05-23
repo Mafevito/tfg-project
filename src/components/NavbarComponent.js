0aa7e29578ed3d0fb9527b1331963b027395659c
Marifer Villarroel <mafevito@hotmail.com>
2023-05-23 17:12:37 +0200

    NavbarComponent configurado con react-router y chakraui

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,30 +1,43 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { LinkContainer } from "react-router-bootstrap";
-import { Outlet } from "react-router-dom";
+import {
+  Flex,
+  HStack,
+  Heading,
+  Spacer,
+  Link,
+  StackDivider,
+  Text,
+  Container,
+} from "@chakra-ui/react";
+import { Outlet, Link as RouteLink } from "react-router-dom";
+
+const NavLink = ({ text }) => (
+  <Link>
+    <Text fontSize="lg">{text}</Text>
+  </Link>
+);
 
 export default function NavbarComponent() {
   return (
-    <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>Logo</Navbar.Brand>
-          </LinkContainer>
+    <Container maxW="1200px">
+      <Flex as="nav" p="20px" mb="80px" alignItems="center" bg="gray.200">
+        <Heading as="h1">
+          <RouteLink to="/">
+            <NavLink text="LOGO" />
+          </RouteLink>
+        </Heading>
+        <Spacer />
 
-          <Nav className="justify-content-end">
-            <LinkContainer to="/login">
-              <Nav.Link>Iniciar sesión</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/register">
-              <Nav.Link>Registrarse</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Container>
-      </Navbar>
+        <HStack spacing={3} divider={<StackDivider />} as="nav">
+          <RouteLink to="/login">
+            <NavLink text="Acceder" />
+          </RouteLink>
+          <RouteLink to="/register">
+            <NavLink text="Registrarse" />
+          </RouteLink>
+        </HStack>
+      </Flex>
 
       <Outlet />
-    </>
+    </Container>
   );
 }
